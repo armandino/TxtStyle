3c3749627fb3926ebd06997b6e1206664fe37906
Arman Sharif <arman.sharif@elasticpath.com>
2012-07-08 09:12:24 +0100

    Refactored code to simplify the string matching.

--- txtstyle/regionmatcher.py
+++ txtstyle/regionmatcher.py
@@ -10,8 +10,8 @@ class RegionMatcher:
         """
         search_tokens = self._get_unique_search_tokens(line, regex_obj)
         found_regions = []
-        for searchstr in search_tokens:
-            matching_regions = self._find_regions(line, searchstr)
+        for token in search_tokens:
+            matching_regions = self._find_regions(line, token)
 
             if matching_regions:
                 found_regions.extend(matching_regions)
@@ -24,6 +24,7 @@ class RegionMatcher:
         Returns a list of unique search tokens that match the regex eg.
         Given "foo 12 bar 34" and regex "\d\d" returns: ['12', '34']
         """
+        
         search_tokens = []
         it = re.finditer(regex_obj, line)
         for match in it:
@@ -36,65 +37,31 @@ class RegionMatcher:
         else:
             return search_tokens
 
-    def _find_regions(self, s, searchstr):
-        regions = []
-        offset = 0
-        remainder = s
-        while True:
-            remainder, offset = self._collect_regions(remainder, searchstr, regions, offset)
-            if not remainder:
-                break
-
-        return regions
-
-    def _collect_regions(self, s, searchstr, results=[], offset=0):
-        if not searchstr:
-            return '', 0
+    def _find_regions(self, string, token):
+        """\
+        Returns a list of regions (start, end) matching the token.
+        """
+        if not token:
+            return []
         
-        if searchstr == s and offset == 0:
-            # if whole string match (when offset > 0, we're processing remainder)
-            results.append((0, len(s) - 1))
-            return '', 0
-
-        start = s.find(searchstr)
-        if start == -1:
-            return '', 0
-
-        # snip non-matching front
-        # e.g. search 'cat' in 'black cat' -> '[black ]cat'
-        remainder = s[start:]
-
-        # remove first occurance of searchstr in s
-        if remainder == searchstr:
-            remainder = ''
-        else:
-            # escape in case searchstr has regex characters in it
-            remainder = re.sub(re.escape(searchstr), '', remainder, 1)
-
-        # Calculate region (start, end)
-        if remainder == '':
-            end = len(s) - 1
-        else:
-            # e.g. search 'o' in 'clock' (2,2)
-            if len(searchstr) == 1:
-                end = start
-            # e.g. search 'is' in 'isisis' (remainder = 'isis')
-            elif s.startswith(remainder):
-                end = len(searchstr) - 1
-            # eg. search 'is' in 'isis'
-            elif remainder == searchstr:
-                end = len(searchstr) * 2 - 1
+        results = []
+        offset = 0
+        string_len = len(string)
+        
+        while True:
+            idxs = string.find(token, offset, string_len)
+            if idxs == -1:
+                break
+            
+            token_len = len(token)
+            if token_len == 1:
+                idxe = idxs
+                offset = idxe + 1
             else:
-                end = s.find(remainder) - 1
+                idxe = idxs + token_len - 1
+                offset = idxe
+            
+            result = (idxs, idxe)
+            results.append(result)
 
-                if (end < start):
-                    end = start - 1
-                    # when searching through multiple occurrences, e.g. '-11' in 'foo-11-11'
-                    while (end < start and end >= 0):
-                        end = s.find(remainder, end + len(remainder), len(s)) - 1
-
-        result = (start + offset, end + offset)
-        results.append(result)
-        offset = offset + end + 1
-        return remainder, offset
-
+        return results
